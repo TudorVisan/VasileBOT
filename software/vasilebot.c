da40f6bec115f9242b3e1c201f3d2530139d5a0f
Liviu-Andrei Mitruta <mitrutaliviu@outlook.com>
2016-05-18 21:17:46 +0300

    Added intrerrupts to software.
    Added intrerrupt specific macros.
    Added IO_init function.

--- software/vasilebot.c
+++ software/vasilebot.c
@@ -1,4 +1,4 @@
-//aprinderea LED-ului de debug
+
 #define F_CPU 16000000
  
 #include <avr/io.h>
@@ -8,29 +8,38 @@
  #define DEBUG_LED_DDR	DDRB
  #define DEBUG_LED_PIN	PINB
  #define DEBUG_LED_PORT	PORTB
- #define DEBUG_LED	PB0
+ #define DEBUG_LED		PB0
  
 /* Buton on/off(BTN1) conectat pe portul PD4 */
  #define BTN1_DDR	DDRD
  #define BTN1_PIN	PIND
  #define BTN1_PORT	PORTD
- #define BTN1	PD4
+ #define BTN1		PD4
+ #define BTN1_PCINT	PCINT20
+ #define BTN1_PCINT_IE	PCIE2
+ #define BTN1_PCMSK PCMSK2
  
 /* Buton lower frequency(BTN2) conectat pe portul PC5 */
  #define BTN2_DDR	DDRC
  #define BTN2_PIN	PINC
  #define BTN2_PORT	PORTC
- #define BTN2	PC5
+ #define BTN2		PC5
+ #define BTN2_PCINT	PCINT13
+ #define BTN2_PCINT_IE	PCIE1
+ #define BTN2_PCMSK PCMSK1
  
 /* Buton higher frequency(BTN3) conectat pe portul PC4 */
  #define BTN3_DDR	DDRC
  #define BTN3_PIN	PINC
  #define BTN3_PORT	PORTC
- #define BTN3	PC4
+ #define BTN3		PC4
+ #define BTN3_PCINT	PCINT12
+ #define BTN3_PCINT_IE	PCIE1
+ #define BTN3_PCMSK PCMSK1
  
-
-
-int main() {
+ 
+void IO_init(void)
+{
 	/* se setează pinul de ieșire al DEBUG_LED */
 	DEBUG_LED_DDR |= (1 << DEBUG_LED);
 	
@@ -47,47 +56,49 @@ int main() {
 	/* stingerea DEBUG_LED */
 	DEBUG_LED_PORT &= ~(1 << DEBUG_LED);
 	
+	PCICR |= (1 << BTN1_PCINT_IE);
+	PCICR |= (1 << BTN2_PCINT_IE);
+	PCICR |= (1 << BTN3_PCINT_IE);
+	
+	BTN1_PCMSK |= (1 << BTN1_PCINT);
+	BTN2_PCMSK |= (1 << BTN2_PCINT);
+	BTN3_PCMSK |= (1 << BTN3_PCINT);
+}
+
+void motor_init(void)
+{
+	// TODO
+}
+
+ISR(PCINT1_vect)
+{
+	if( ( BTN3_PIN & ( 1 << BTN3) == 0 )
+		if( freq < fmax )
+			freq++;
+	
+	if( ( BTN2_PIN & ( 1 << BTN2) == 0 )
+		if( freq > fmin )
+			freq--;
+}
+
+ISR(PCINT2_vect)
+{
+	if( ( BTN1_PIN & ( 1 << BTN1 ) ) == 0 )
+		DBG_LED_GO ^= 1;
+}
+
+
+int main() 
+{
+	IO_init();
+	
+	sei();
+	
 	int freq = 1000, fmax = 4999, fmin = 9;
-	/* flags for BTN 1,2,3 */
-	int BTN2_Prev_pressed = 0, BTN3_Prev_pressed = 0, BNT1_Prev_Pressed = 0, DBG_LED_GO = 0;
 	
 	while(1) 
 	{
-		if( ( BTN1_PIN & ( 1 << BTN1 ) ) == 0 )
-			if ( !BTN1_Prev_pressed )
-			{
-				BNT1_Prev_Pressed = 1;
-				DBG_LED_GO ^= 1;
-			}
-		else
-			BNT1_Prev_Pressed = 0;
-		
-		if( ( BTN3_PIN & ( 1 << BTN3) == 0 )
-		{
-			if ( !BTN3_Prev_pressed )
-			{
-				BTN3_Prev_pressed = 1;
-				if( freq < fmax )
-					freq++;
-			}			
-		}			
-		else
-			BTN3_Prev_pressed = 0;
-		
-		if( ( BTN2_PIN & ( 1 << BTN2) == 0 )
-		{
-			if( !BTN2_Prev_pressed )
-			{
-				BTN2_Prev_pressed = 1;
-				if( freq > fmin )
-					freq--;
-			}
-		}
-		else 
-			BTN2_Prev_pressed = 0;
-		
-		if( DBG_LED_GO )
-			DEBUG_LED_PORT ^= (1 << DEBUG_LED);
+		DEBUG_LED_PORT ^= (1 << DEBUG_LED);
 		_delay_ms(freq);
 	}
  
